9407401a74eee14ac740bfaa05eb1b1a834defdd
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-06-12 14:00:34 +0000

    First integration test for the default welcome controller

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
-    belongs_to :user, index: true
-    belongs_to :car, index: true
+    belongs_to :user, foreign_key: true
+    belongs_to :car, foreign_key: true
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,4 +4,13 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test 'assert_react_component' do 
+    get '/' 
+    assert_equal 200, response.status 
+
+    assert_react_component "HelloWorld" do |props|
+      assert_equal "Hello from react-rails.", props[:greeting]
+  end
+
 end
+end 
