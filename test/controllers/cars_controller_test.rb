d73719a497203712995932892ab0955d7df8eeaa
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-06-15 12:41:26 +0000

    remove the default test folder

--- test/controllers/cars_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class CarsControllerTest < ActionDispatch::IntegrationTest
-  #  test "the cars controller " do
-  #    get root_path
-  #    expect(page).to have_content('Range Rover')
-  #  end
-
-end
--- test/models/car_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-class CarTest < ActiveSupport::TestCase
-  test "the car is valid" do 
-    car = Car.new(model: "Toyota", image: "Image", description: "Toyota Land Cruiser")
-    assert car.save
-  end 
-end
