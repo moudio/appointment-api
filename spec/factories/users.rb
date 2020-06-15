09ae802dd0899c5bd454663d438e552b91e7039e
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-06-15 13:56:26 +0000

    configure factory bot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :user do
-    Car { "yString" }
-    Book { "MyString" }
+    Car { "Range Rover" }
+    Book { "1" }
+    username {"user"}
+    password {"password"}
+    password_confirmation {"password"}
   end
 end
--- spec/requests/cars_request_spec.rb
+++ spec/requests/cars_request_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "Cars", type: :request do
+RSpec.describe "Cars API", type: :request do
+    let!(:cars) {create_list(:car, 5)}
+    let(:car_id) {cars.first.id}
 
+    describe 'Get /cars' do 
+        before {get 'todos'}
+    
+        it 'returns cars' do 
+                expect(json).not_to be_empty
+                expect(json.size).to eq(10)
+        end 
+    end 
 end
