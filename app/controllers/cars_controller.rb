446efd1f1dc082831c27445f2aeb1cd4d1ed6d3b
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-06-15 12:05:33 +0000

    change the controller to return json and xml format

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,5 +2,10 @@ class CarsController < ApplicationController
     before_action :authorized, only: [:show]
     def index 
         @cars = Car.all
+        respond_to do |format|
+            format.html
+            format.xml {render :xml => @cars }
+            format.json {render :json => @cars }
+        end 
     end 
 end
