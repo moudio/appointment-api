7e18608ce6d2ed9135b97df0e7cf653913d56348
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-06-16 10:08:23 +0000

    Add PUT test suite for car

--- spec/requests/cars_request_spec.rb
+++ spec/requests/cars_request_spec.rb
@@ -60,7 +60,44 @@ RSpec.describe "Cars API", type: :request do
             it "creates a car" do 
                 expect(JSON(response.body['model'])).to eq('Range Rover Auto')
             end 
+
+            it "returns status code 201" do 
+                expect(response).to have_http_status(201)
+            end 
+        end
+        
+        context 'when the request is invalid' do 
+            before {post '/cars', params: {model: 'Fake car'}}
+
+            it "returns status code 422" do 
+                expect(response).to have_http_status(422);
+            end 
+
+            it 'returns a validation failure message' do 
+                    expect(response).body
+                    .to match(/Validation failed: Description can't be blank/)
+            end 
+        end 
+
+        
+
+    end 
+
+    describe 'PUT /cars/:id' do 
+
+        let(:valid_attributes) {{model: 'Range Rover Velar'}}
+
+        context 'when the record exists' do 
+
+            before {put "/cars/#{car_id}"}
+
+            it "returns status code 204" do 
+                expect(response).to have_http_status(204)
+            end 
+        
+        
         end 
     end 
 
-end
+
+end 
