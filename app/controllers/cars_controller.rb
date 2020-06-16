8fa02e29fd95b614ec904e3a5cebf888377efdc5
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-06-16 10:31:00 +0000

    Add the car controller actions

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,15 +1,39 @@
 class CarsController < ApplicationController
     # before_action :authorized
+    before_action :set_car, only: [:show, :update, :destroy]
     def index 
-        render :json => Car.all
+        @cars = Car.all 
+        json_response(@cars)
+    end 
+
+
+    def create 
+        @car = Car.create!(car_params)
+        json_response(@car, :created)
     end 
 
     def show 
-        
-        render :json => Car.find(params[:id])
+        json_response(@car)
+    end 
+
+    def update 
+    @car.update(car_params)
+    head :no_content 
+    end 
+
+    def destroy 
+        @car.destroy 
+        head :no_content 
     end 
 
 
+    private 
 
+    def car_params 
+        params.require(:car).permit(:model, :description, :alt)
+    end 
 
+    def set_todo 
+        @car = Car.find(params[:id])
+    end 
 end
