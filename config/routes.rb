8692426a978f8f6a6b19e94e585dba6fe89f6a6d
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-06-16 13:44:43 +0000

    Add nested resources for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
-
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:create, :index, :show]
+  resources :users do 
+    resources :cars , only: [:show]
+    resources :books
+  end 
   resources :cars
   resources :books
 
