1b3bb0e8e6f8f2969cfafe2b82a000e89f740bc6
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-06-16 11:04:19 +0000

    Create the user request API test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
       before_action :authorized, only: [:show]
+    
     def new 
     @user = User.new
     end 
---
+++ spec/requests/users_request_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "Users API", type: :request do
+
+    
+
+end 
