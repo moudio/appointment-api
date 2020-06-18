5996eca6abe38b7c107d5b0cf33bab1b5643deee
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-06-18 14:38:56 +0000

    Add the new routes to the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,16 @@ scope '/api/v1' do
   resources :cars
   resources :users
   resources :books
-end 
+end
   root 'cars#index'
   get '/welcome' => 'pages#welcome'
   get '/signup' => 'users#new'
   get '/login' => 'sessions#new'
   post '/sessions' => 'sessions#create'
-  delete '/sessions' => 'sessions#destroy'
+  delete '/logout' => 'sessions#destroy'
   get '/404' => 'errors#not_found'
   get '/users/:id/cars' => "users#user_cars"
   get '/users/:id/books' => "users#user_books"
   get '/users/:id/cars/:car_id' => "users#show_car"
-
+  get 'logged_in', to: 'sessions#is_logged_in?'
 end
