a68bb3ae4f190b467a38f6eeeb01ef90de6347d1
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-06-22 13:40:38 +0000

    update the bookings permitted params to accept ciy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,6 +19,6 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.require(:book).permit(:user_id, :car_id, :booking_date)
+    params.require(:book).permit(:user_id, :car_id, :booking_date, :city)
   end
 end
