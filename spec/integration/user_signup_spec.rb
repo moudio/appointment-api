d3079ad79ecc714698c25caef0b708aa3bbae126
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-06-26 17:52:57 +0000

    Successfully logging in user

--- spec/integration/user_signup_spec.rb
+++ spec/integration/user_signup_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
 
-feature 'user signup' do 
-scenario "user signs up with validation" do 
-visit signup_path
-fill_in "Username", with: "Mouhamadou"
-fill_in "Password", with: "password"
-fill_in "Password confirmation", with: "password"
+RSpec.describe "Users" , type: :request do
+describe 'Signing up' do
 
-expect do 
-click_button 'Create Account'
-end.to change(User, :count).by(1) 
-end 
-end 
+
+  context "craeting a new user" do
+
+    before {post '/api/v1/users', params: {user: {username: "NewUser", password: "password"}}}
+      it "returns credentials for created user" do
+        expect(JSON(response.body)['status']).to eq('created')
+      end
+
+    end
+
+  end
+
+end
