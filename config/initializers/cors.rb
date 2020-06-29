ac3d7b249adc2eeff3efd1de7900ae0c67f19461
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-06-29 11:34:44 +0000

    change the cors  origins to the front-end app website

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://appointcar.netlify.app/'
     resource '*', headers: :any, methods: %i[get post patch put delete options head],
                   credentials: true
   end
