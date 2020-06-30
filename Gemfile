a1c650b4c228830c59ed5afd43897989c68d0eec
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-06-30 12:28:32 +0000

    delete string for Rack:Cors

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ ruby '2.7.0'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.1'
 # Use sqlite3 as the database for Active Record
 # Use Puma as the app server
-gem 'rack-cors', '~> 0.3.1'
+gem 'rack-cors'
 
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Appointment
     #     'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
     #   }
 
-    config.middleware.insert_before ActionDispatch::Static, "Rack::Cors" do
+    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
       allow do
         origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
