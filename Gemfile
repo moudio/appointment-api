5936acb82a6df89952eacbc4827789a33435bdc1
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-06-30 12:24:48 +0000

    update the access control origin allowed

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ ruby '2.7.0'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.1'
 # Use sqlite3 as the database for Active Record
 # Use Puma as the app server
+gem 'rack-cors', '~> 0.3.1'
+
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
 gem 'rubocop', '~> 0.85.1', require: false
--- config/application.rb
+++ config/application.rb
@@ -15,12 +15,17 @@ module Appointment
     config.load_defaults 5.2
     config.assets.initialize_on_precompile = false
     config.assets.initialize_on_precompile = false
-    config.action_dispatch.default_headers = {
-        'Access-Control-Allow-Origin' => '*',
-        'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
-      }
+    # config.action_dispatch.default_headers = {
+    #     'Access-Control-Allow-Origin' => '*',
+    #     'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    #   }
 
-
+    config.middleware.insert_before ActionDispatch::Static, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
