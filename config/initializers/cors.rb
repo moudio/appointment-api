2568d3be480902d9d962fb435974c12b2bfbefe5
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-06-30 09:43:54 +0000

    change the cors file origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,7 @@
-
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+    origins 'https://appointcar.netlify.app/'
+    resource '*', headers: :any, methods: %i[get post patch put delete options head],
+                  credentials: true
   end
 end
