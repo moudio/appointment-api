21a07f1b13f24dc6931e0e84dcf7807bb3c0c8ff
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-06-30 10:05:45 +0000

    Update the confi.run for heroku deploy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'https://appointcar.netlify.app/'
-    resource '*', headers: :any, methods: %i[get post patch put delete options head],
-                  credentials: true
-  end
-end
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins 'https://appointcar.netlify.app/'
+#     resource '*', headers: :any, methods: %i[get post patch put delete options head],
+#                   credentials: true
+#   end
+# end
