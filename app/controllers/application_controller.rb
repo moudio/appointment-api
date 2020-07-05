47e3befd4937d2ff04bec48a6e97685dee8f1a69
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-07-05 10:21:48 +0000

    add the same_site attribute for cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < ActionController::API
     cookies.encrypted[:appointcar] = {
       value: @user.id,
       expires: 1.day.from_now,
+      same_site: 'Lax'
     }
   end
 
