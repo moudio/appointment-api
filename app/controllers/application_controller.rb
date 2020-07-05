c10413400b4c211cf91645f04ec21bb35c2ff301
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-07-05 10:27:37 +0000

    Add secure to same_site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::API
     cookies.encrypted[:appointcar] = {
       value: @user.id,
       expires: 1.day.from_now,
-      same_site: 'None'
+      same_site: 'Secure'
     }
   end
 
